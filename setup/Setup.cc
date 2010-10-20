3a9fd45054c072a4ae3040eec1c23992e96b41aa
Bastian Beischer <beischer@physik.rwth-aachen.de>
2010-10-20 15:57:12 +0200

    For the moment we can only construct SiPMArrays

--- setup/Setup.cc
+++ setup/Setup.cc
@@ -1,6 +1,7 @@
 #include "Setup.hh"
 
 #include "DetectorElement.hh"
+#include "SiPMArray.hh"
 
 Setup* Setup::m_instance = 0;
 
@@ -21,6 +22,6 @@ Setup* Setup::instance()
 // this should differentiate between types of detector elements soon
 DetectorElement* Setup::element(unsigned short id)
 {
-  if (!m_elements[id]) m_elements[id] = new DetectorElement;
+  if (!m_elements[id]) m_elements[id] = new SiPMArray;
   return m_elements[id];
 }
