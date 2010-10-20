d426dd6893c5ee3b11a2d316d953cc2ab6eea9a9
Bastian Beischer <beischer@physik.rwth-aachen.de>
2010-10-20 13:41:45 +0200

    preparing DetectorElement map, doesn't compile yet

--- setup/Setup.cc
+++ setup/Setup.cc
@@ -1,5 +1,7 @@
 #include "Setup.hh"
 
+#include "DetectorElements.hh"
+
 Setup* Setup::m_instance = 0;
 
 Setup::Setup()
@@ -14,4 +16,10 @@ Setup* Setup::instance()
 {
   if (!m_instance) m_instance = new Setup;
   return m_instance;
+}
+
+DetectorElement* Setup::element(unsigned short id)
+{
+  if (!m_elements[id]) m_elements[id] = new DetectorElement;
+  return m_elements[id];
 }
--- setup/Setup.hh
+++ setup/Setup.hh
@@ -1,19 +1,28 @@
 #ifndef Setup_hh
 #define Setup_hh
 
+#include <QMap>
+
+class DetectorElement;
+
 class Setup
 {
   
 public:
-  static Setup* instance();
   ~Setup();
+
+  static Setup* instance();
   
+  DetectorElement* element(unsigned short id);
+
 private:
   Setup();
   
 private:
   static Setup* m_instance;
-  
+
+  QMap<unsigned short, DetectorElement*> m_elements;
+
 };
 
 #endif /* Setup_hh */
