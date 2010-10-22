da790a98ca9ececca7d84562d91e527cc0d650d1
Bastian Beischer <beischer@physik.rwth-aachen.de>
2010-10-22 19:31:45 +0200

    fixed indentation

--- tools/preAnalysis/main.cc
+++ tools/preAnalysis/main.cc
@@ -22,39 +22,39 @@ int main(int argc, char** argv)
   if (!runFileListFile.open(QIODevice::ReadOnly | QIODevice::Text))
     qFatal("Could not open run file list file.");
 	
-	TFile destinationTreeFile(qPrintable(QString(argv[1])+".root"), "RECREATE");
-	TTree destinationTree("SimpleEventTree", "tree with simple events");
-	SimpleEvent* destinationEvent = 0;
-	destinationTree.Branch("event", "SimpleEvent", &destinationEvent);
-	DataDescription description;
+  TFile destinationTreeFile(qPrintable(QString(argv[1])+".root"), "RECREATE");
+  TTree destinationTree("SimpleEventTree", "tree with simple events");
+  SimpleEvent* destinationEvent = 0;
+  destinationTree.Branch("event", "SimpleEvent", &destinationEvent);
+  DataDescription description;
 	
-	QTextStream inStream(&runFileListFile);
+  QTextStream inStream(&runFileListFile);
   QString line = inStream.readLine();
   while (!line.isNull()) {
-		qDebug() << line;
-		description.addRunFile(line.toStdString());
-		gROOT->cd();
+    qDebug() << line;
+    description.addRunFile(line.toStdString());
+    gROOT->cd();
     TFile sourceTreeFile(qPrintable(line));
-		if (sourceTreeFile.IsZombie())
-			qFatal("Could not open run file.");
-		TTree* sourceTree = static_cast<TTree*>(sourceTreeFile.Get("SimpleEventTree"));
-		SimpleEvent* sourceEvent = 0;
-		sourceTree->SetBranchAddress("event", &sourceEvent);
-		for (long eventIt = 0; eventIt < sourceTree->GetEntries(); ++eventIt) {
-			sourceTree->GetEntry(eventIt);
+    if (sourceTreeFile.IsZombie())
+      qFatal("Could not open run file.");
+    TTree* sourceTree = static_cast<TTree*>(sourceTreeFile.Get("SimpleEventTree"));
+    SimpleEvent* sourceEvent = 0;
+    sourceTree->SetBranchAddress("event", &sourceEvent);
+    for (long eventIt = 0; eventIt < sourceTree->GetEntries(); ++eventIt) {
+      sourceTree->GetEntry(eventIt);
 
 
 
-			destinationEvent = sourceEvent;
-			destinationTree.Fill();
-		}
+      destinationEvent = sourceEvent;
+      destinationTree.Fill();
+    }
     line = inStream.readLine();
     sourceTreeFile.Close();
   }
-	destinationTree.GetUserInfo()->Add(&description);
-	destinationTreeFile.cd();
-	destinationTree.Write();
-	destinationTreeFile.Close();
+  destinationTree.GetUserInfo()->Add(&description);
+  destinationTreeFile.cd();
+  destinationTree.Write();
+  destinationTreeFile.Close();
 
   return EXIT_SUCCESS;
 }
