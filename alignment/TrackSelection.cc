17550157b78a03364b66414c4692ad097e426ec7
Bastian Beischer <beischer@physik.rwth-aachen.de>
2010-10-27 14:41:15 +0200

    working on track selection

--- alignment/TrackSelection.cc
+++ alignment/TrackSelection.cc
@@ -2,15 +2,42 @@
 
 #include "Track.hh"
 
+#include <QRectF>
+
 TrackSelection::TrackSelection()
 {
+  constructPlanes();
 }
 
 TrackSelection::~TrackSelection()
 {
 }
 
-bool TrackSelection::passes(Track* /*track*/)
+void TrackSelection::constructPlanes()
 {
-  return true;
+  double width = 150.;
+  double height = 200.;
+  QRectF rect(QPointF(-width/2., -height/2.), QPointF(width/2., height/2.));
+
+  // upper magnet boundary
+  m_planes.push_back(Plane( 100., rect));
+  // lower magnet boundary
+  m_planes.push_back(Plane(-100., rect));
+}
+
+bool TrackSelection::passes(Track* track)
+{
+  bool passes = true;
+
+  foreach(Plane plane, m_planes) {
+    double z = plane.first;
+    double x = track->x(z);
+    double y = track->y(z);
+
+    QRectF rectangle = plane.second;
+    if (rectangle.contains(x,y))
+      passes = false;
+                   
+  }
+  return passes;
 }
