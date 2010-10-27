a4ed95d19e626faf59177d46c58e90d2c8ad176c
Bastian Beischer <beischer@physik.rwth-aachen.de>
2010-10-27 17:36:11 +0200

    fixed indentation

--- event/TOFSipmHit.cc
+++ event/TOFSipmHit.cc
@@ -6,10 +6,10 @@
 ClassImp( TOFSipmHit );
 
 TOFSipmHit::TOFSipmHit()
-	: Hit()
-	, m_startTime(0)
-	, m_timeOverThreshold(0)
-	, m_levelChanges()
+  : Hit()
+  , m_startTime(0)
+  , m_timeOverThreshold(0)
+  , m_levelChanges()
 {}
 
 TOFSipmHit::~TOFSipmHit()
@@ -17,65 +17,64 @@ TOFSipmHit::~TOFSipmHit()
 
 void TOFSipmHit::processTDCHits()
 {
-	//TODO: Use TOT to get the wanted signal pusle.
-	m_startTime = time(0);
-	//TODO: What if there is a noise pulse?
-	m_timeOverThreshold = time(1) - time(0);
+  //TODO: Use TOT to get the wanted signal pusle.
+  m_startTime = time(0);
+  //TODO: What if there is a noise pulse?
+  m_timeOverThreshold = time(1) - time(0);
 }
 
 double TOFSipmHit::time(int i) const
 {
-	assert(0 <= i && i < numberOfLevelChanges());
-	return timeFromData(m_levelChanges[i]);
+  assert(0 <= i && i < numberOfLevelChanges());
+  return timeFromData(m_levelChanges[i]);
 }
 
 TOFSipmHit::EdgeType TOFSipmHit::edgeType(int i) const
 {
-	assert(0 <= i && i < numberOfLevelChanges());
+  assert(0 <= i && i < numberOfLevelChanges());
   return (m_levelChanges[i] & 0x02000000) ? TrailingEdge: LeadingEdge;
 }
 
 int TOFSipmHit::channel(int i) const
 {
-	assert(0 <= i && i < numberOfLevelChanges());
-	return channelFromData(m_levelChanges[i]);
+  assert(0 <= i && i < numberOfLevelChanges());
+  return channelFromData(m_levelChanges[i]);
 }
 
 int TOFSipmHit::spillNumber(int i) const
 {
-	assert(0 <= i && i < numberOfLevelChanges());
-	return (m_levelChanges[i] >> 26) & 0x3F;
+  assert(0 <= i && i < numberOfLevelChanges());
+  return (m_levelChanges[i] >> 26) & 0x3F;
 }
 
 int TOFSipmHit::channelFromData(uint32_t value)
 {
-	return (value & 0x01F80000) >> 19;
+  return (value & 0x01F80000) >> 19;
 }
 
 double TOFSipmHit::timeFromData(uint32_t value)
 {
-	return 10. * (value & 0x0007FFFF);
+  return 10. * (value & 0x0007FFFF);
 }
 
 bool TOFSipmHit::earlierThan(uint32_t data1, uint32_t data2)
 {
-	if (channelFromData(data1) < channelFromData(data2))
-		return true;
-	if (timeFromData(data1) < timeFromData(data2))
-		return true;
-	return false;
+  if (channelFromData(data1) < channelFromData(data2))
+    return true;
+  if (timeFromData(data1) < timeFromData(data2))
+    return true;
+  return false;
 }
 
 void TOFSipmHit::setLevelChange(int i, uint32_t value)
 {
-	assert(0 <= i && i < numberOfLevelChanges());
-	m_levelChanges[i] = value;
-	std::sort(m_levelChanges.begin(), m_levelChanges.end(), earlierThan);
+  assert(0 <= i && i < numberOfLevelChanges());
+  m_levelChanges[i] = value;
+  std::sort(m_levelChanges.begin(), m_levelChanges.end(), earlierThan);
 }
 
 void TOFSipmHit::addLevelChange(uint32_t value)
 {
-	m_levelChanges.push_back(0);
-	setLevelChange(numberOfLevelChanges() - 1, value);
+  m_levelChanges.push_back(0);
+  setLevelChange(numberOfLevelChanges() - 1, value);
 }
-
