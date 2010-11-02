c7a7fc3d687462d69211cf7cf9ad552e856dc239
Bastian Beischer <beischer@physik.rwth-aachen.de>
2010-11-02 19:31:15 +0100

    assert if not all of the channels are present

--- setup/DetectorElement.cc
+++ setup/DetectorElement.cc
@@ -2,6 +2,8 @@
 
 #include <QtAlgorithms>
 
+#include <cassert>
+
 bool comparePositions(const Hit* hit1, const Hit* hit2)
 {
   return hit1->position().x() < hit2->position().x();
@@ -27,6 +29,14 @@ DetectorElement::~DetectorElement()
 
 void DetectorElement::sortHits()
 {
+  // check if all channels are present --> otherwise reinserting into map will be wrong if a channel was skipped
+  if (m_type == tracker)
+    assert(m_hits.size() == 32);
+  else if (m_type == trd)
+    assert(m_hits.size() == 16);
+  // else if (m_type == tof)
+  //   assert(hits.size() == 4);
+
   QList<Hit*> hits = m_hits.values();
   qSort(hits.begin(), hits.end(), comparePositions);
   for (int i = 0; i < hits.size(); i++) {
