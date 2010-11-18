9e5d648e2fbef38029b840bae8a1f6717cf06d49
Bastian Beischer <beischer@physik.rwth-aachen.de>
2010-11-18 01:57:01 +0100

    fixed bug with Spectrum and too many histograms

--- tools/analysis/MomentumSpectrumPlot.cc
+++ tools/analysis/MomentumSpectrumPlot.cc
@@ -19,16 +19,6 @@ MomentumSpectrumPlot::MomentumSpectrumPlot() :
   histogram->GetYaxis()->SetTitle("entries");
   histogram->SetLineColor(kBlack);
   addHistogram(histogram);
-  histogram = new TH1D(qPrintable(title()), "", 100, -20, 20);
-  histogram->GetXaxis()->SetTitle("p / GeV");
-  histogram->GetYaxis()->SetTitle("entries");
-  histogram->SetLineColor(kRed);
-  addHistogram(histogram);
-  histogram = new TH1D(qPrintable(title()), "", 100, -20, 20);
-  histogram->GetXaxis()->SetTitle("p / GeV");
-  histogram->GetYaxis()->SetTitle("entries");
-  histogram->SetLineColor(kBlue);
-  addHistogram(histogram);
 }
 
 MomentumSpectrumPlot::~MomentumSpectrumPlot()
@@ -50,12 +40,9 @@ void MomentumSpectrumPlot::processEvent(const QVector<Hit*>& hits, Track* track,
   double pt = track->pt();
 
   double r = sqrt(track->x(0)*track->x(0) + track->y(0)*track->y(0));
-  //  histogram(0)->Fill(pt);
   if (r < 75) {
     histogram(0)->Fill(pt);
   }
-  //  if (r > 140)
-    //    histogram(2)->Fill(pt);
 }
 
 void MomentumSpectrumPlot::finalize()
