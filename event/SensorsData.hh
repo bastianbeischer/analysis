ed84eae9c1f4bd01f2d46616b5bca09c87c20126
Bastian Beischer <beischer@physik.rwth-aachen.de>
2010-12-16 14:38:46 +0100

    Set indendation offset to "2"

--- event/SensorsData.hh
+++ event/SensorsData.hh
@@ -9,32 +9,32 @@
 
 class SensorsData {
 public:
-	enum DataType { SENSORS, ATC /*,EBASS*/ };
+  enum DataType { SENSORS, ATC /*,EBASS*/ };
 	
 public:
-	SensorsData();
-	bool setFile(const char* file);
-	float getPrevious(DataType type, const char* id, time_t time, unsigned int *diff);
-	float getNext(DataType type, const char* id, time_t time, unsigned int *diff);
-	float getAverage(DataType type, const char* id, time_t time); // Will return NaN if out of bounds.
-	std::map<unsigned int,float> getValues(DataType type, const char* id);
+  SensorsData();
+  bool setFile(const char* file);
+  float getPrevious(DataType type, const char* id, time_t time, unsigned int *diff);
+  float getNext(DataType type, const char* id, time_t time, unsigned int *diff);
+  float getAverage(DataType type, const char* id, time_t time); // Will return NaN if out of bounds.
+  std::map<unsigned int,float> getValues(DataType type, const char* id);
 	
 	
-	char** getKeys(DataType type);
-	int getNumberOfKeys(DataType type);
+  char** getKeys(DataType type);
+  int getNumberOfKeys(DataType type);
 	
 private:
-	std::map<unsigned int,unsigned int> m_sensorstimes;
-	std::map<unsigned int,unsigned int> m_atctimes;
-	std::map<unsigned int,unsigned int> m_ebasstimes;
-	TTree* m_sensorstree;
-	TTree* m_atctree;
-	TTree* m_ebasstree;
-	TFile* m_file;
-	bool m_init;
+  std::map<unsigned int,unsigned int> m_sensorstimes;
+  std::map<unsigned int,unsigned int> m_atctimes;
+  std::map<unsigned int,unsigned int> m_ebasstimes;
+  TTree* m_sensorstree;
+  TTree* m_atctree;
+  TTree* m_ebasstree;
+  TFile* m_file;
+  bool m_init;
 	
-	TTree* getTree(DataType type);
-	std::map<unsigned int, unsigned int> getMap(DataType type);
+  TTree* getTree(DataType type);
+  std::map<unsigned int, unsigned int> getMap(DataType type);
 	
 };
 
