c73c9c6c46d11c001df8aac80ddc0ee87f3d3c97
David Schug <schug@physik.rwth-aachen.de>
2011-01-06 16:17:51 +0100

    added the trd efficiency plot classes

---
+++ tools/analysis/TRDEfficiencyPlot.hh
@@ -0,0 +1,40 @@
+#ifndef TRDEFFICIENCYPLOT_HH
+#define TRDEFFICIENCYPLOT_HH
+
+#include "AnalysisPlot.hh"
+#include "H2DPlot.hh"
+
+#include "TEllipse.h"
+
+#include <QObject>
+#include <QMap>
+#include <QTime>
+#include <QTimer>
+#include <QVector2D>
+
+class TRDEfficiencyPlot : public QObject, public AnalysisPlot, public H2DPlot
+{
+
+public:
+  TRDEfficiencyPlot();
+  ~TRDEfficiencyPlot();
+  virtual void processEvent(const QVector<Hit*>&, Track* = 0, SimpleEvent* = 0);
+  virtual void finalize();
+  virtual void draw(TCanvas* c);
+  virtual void update();
+
+private:
+  QMap< unsigned short, unsigned long > m_hits;
+  QMap< unsigned short, unsigned long > m_wasOnTrack;
+  QMap< unsigned short, TEllipse* > m_ellipses;
+
+  QMap< unsigned short, QVector2D > m_trdChanPositions;
+
+  bool m_ellipsesDrawn;
+
+  void initializeEllipses();
+  void updateEllipses();
+
+};
+
+#endif // TRDEFFICIENCYPLOT_HH
