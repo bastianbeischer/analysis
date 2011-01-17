8bab295c6ec99e81ca33257d4058f290ce78b8c4
Bastian Beischer <beischer@physik.rwth-aachen.de>
2011-01-17 20:34:43 +0100

    Changed spectrum plot to logarithmic display (for positive charge only at the moment).

--- tools/analysis/MomentumSpectrumPlot.cc
+++ tools/analysis/MomentumSpectrumPlot.cc
@@ -8,6 +8,7 @@
 #include "Track.hh"
 
 #include <iostream>
+#include <cmath>
 
 MomentumSpectrumPlot::MomentumSpectrumPlot(Range range) :
   AnalysisPlot(AnalysisPlot::MomentumReconstruction),
@@ -16,20 +17,33 @@ MomentumSpectrumPlot::MomentumSpectrumPlot(Range range) :
 {
   setTitle("Momentum Spectrum");
 
-  int lowerBound = -20;
-  int upperBound = 20;
+  int nBins = 100;
+  double lowerBound = -20;
+  double upperBound = 20;
+  TH1D* histogram = 0;
   if (m_range == All) {
     setTitle(title() + " - All");
+    histogram = new TH1D(qPrintable(title()), "", nBins, lowerBound, upperBound);
   }
   else if (m_range == Negative) {
     setTitle(title() + " - Negative");
     upperBound = -1e-5;
+    histogram = new TH1D(qPrintable(title()), "", nBins, lowerBound, upperBound);
   }
   else if (m_range == Positive) {
     setTitle(title() + " - Positive");
-    lowerBound = 1e-5;
+    lowerBound = 2*1e-1;
+    std::cout << "ln(upper) = " << log(upperBound) << std::endl;
+    std::cout << "ln(lower) = " << log(lowerBound) << std::endl;
+    double delta = 1./nBins * (log(upperBound)/log(lowerBound) - 1);
+    std::cout << "delta = " << delta << std::endl;
+    double p[nBins+1];
+    for (int i = 0; i < nBins+1; i++) {
+      p[i] = pow(lowerBound, delta*i+1);
+      std::cout << p[i] << std::endl;
+    }
+    histogram = new TH1D(qPrintable(title()), "", nBins, p);
   }
-  TH1D* histogram = new TH1D(qPrintable(title()), "", 100, lowerBound, upperBound);
   histogram->GetXaxis()->SetTitle("p / GeV");
   histogram->GetYaxis()->SetTitle("entries");
   histogram->SetLineColor(kBlack);
