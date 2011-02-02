a0b6947e7788b891bc13acb8a115336e8d496110
Bastian Beischer <beischer@physik.rwth-aachen.de>
2011-02-02 18:07:58 +0100

    Move whole HPE boards in alignment.

--- alignment/Parameters.cc
+++ alignment/Parameters.cc
@@ -50,14 +50,50 @@ void Parameters::reInitParameterArrays(unsigned int nPar)
 // very lol.
 unsigned int Parameters::indexForDetId(unsigned short detId) const
 {
-  Setup* setup = Setup::instance();
+  detId &= 0xFF00;
+
   int index = 0;
-  DetectorElement* element = setup->firstElement();
-  while(element) {
-    if (element->id() == detId) break;
-    element = setup->nextElement();
-    index++;
-  }
+
+  // TOF
+  if (detId == 0x8000) index = 0;
+
+  // Tracker e-side
+  if (detId == 0x6100) index = 1;
+  if (detId == 0x6400) index = 2;
+  if (detId == 0x6000) index = 3;
+  if (detId == 0x6200) index = 4;
+  if (detId == 0x6500) index = 5;
+  if (detId == 0x6700) index = 6;
+  if (detId == 0x6600) index = 7;
+  if (detId == 0x3300) index = 8;
+  if (detId == 0x3700) index = 9;
+  if (detId == 0x6300) index = 10;
+
+  // Tracker g-side
+  if (detId == 0x7B00) index = 11;
+  if (detId == 0x7F00) index = 12;
+  if (detId == 0x7A00) index = 13;
+  if (detId == 0x7E00) index = 14;
+  if (detId == 0x7900) index = 15;
+  if (detId == 0x7D00) index = 16;
+  if (detId == 0x7800) index = 17;
+  if (detId == 0x7C00) index = 18;
+  if (detId == 0x3100) index = 19;
+  if (detId == 0x3000) index = 20;
+
+  // TRD
+  if (detId == 0x3200) index = 21;
+  if (detId == 0x3600) index = 22;
+  if (detId == 0x3400) index = 23;
+  if (detId == 0x3500) index = 24;
+
+  // Setup* setup = Setup::instance();
+  // DetectorElement* element = setup->firstElement();
+  // while(element) {
+  //   if (element->id() == detId) break;
+  //   element = setup->nextElement();
+  //   index++;
+  // }
+
   return index;
 }
-
