0bdc96a5941d240f95e713c700889a6b26d44431
David Schug <schug@physik.rwth-aachen.de>
2011-02-08 12:14:17 +0100

    added scripts to switch between MC and normal conf

---
+++ conf/reset_conf_dir.sh
@@ -0,0 +1,3 @@
+echo "reseting config files"
+git checkout mc_coordinates.conf mc_setup.conf mc_TRDCorrections.conf perdaix_coordinates.conf setup.conf TOFCorrections.conf TRDCorrections.conf
+echo "done"
