7098ced2310adf250d009148e07b8fe28e77323f
Bastian Beischer <beischer@physik.rwth-aachen.de>
2011-02-08 18:53:44 +0100

    Moved scripts to bin directory and added environment variable to absolute path, so that they can be used from everywhere.

---
+++ bin/reset_conf_dir.sh
@@ -0,0 +1,3 @@
+echo "reseting config files"
+git checkout -- $PERDAIXANA_PATH/conf
+echo "done"
--- conf/reset_conf_dir.sh
+++
@@ -1,3 +0,0 @@
-echo "reseting config files"
-git checkout mc_coordinates.conf mc_setup.conf mc_TRDCorrections.conf perdaix_coordinates.conf setup.conf TOFCorrections.conf TRDCorrections.conf
-echo "done"
