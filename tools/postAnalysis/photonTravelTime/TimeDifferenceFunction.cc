f69d0164d2b21f1ef9c23574ef5c9f7fad8e3e23
Roman Greim <greim@physik.rwth-aachen.de>
2011-02-09 20:15:13 +0100

    Fitted photon travel time corrections.

--- tools/postAnalysis/photonTravelTime/TimeDifferenceFunction.cc
+++ tools/postAnalysis/photonTravelTime/TimeDifferenceFunction.cc
@@ -39,8 +39,10 @@ TimeDifferenceFunction::TimeDifferenceFunction(TCanvas* canvas)
   double minY = histogram->GetYaxis()->GetXmin();
   double maxY = histogram->GetYaxis()->GetXmax();
   m_function = new TF2(qPrintable(title), photonTime, minX, maxX, minY, maxY, Corrections::numberOfPhotonTravelTimeParameters);
-  m_function->SetParameters(0, 3, 2);
-  histogram->Fit(m_function, "QN0");
+  m_function->SetParameters(0, 1.5, 2);
+  histogram->Fit(m_function, "QN0 WW");
+  for (int i = 0; i < Corrections::numberOfPhotonTravelTimeParameters; ++i)
+    qDebug() << QString("%1_c%2=%3").arg(title).arg(i).arg(m_function->GetParameter(i));
   title = QString("%1 histogram").arg(canvas->GetName());
   setTitle(title);
   int nBinsX = histogram->GetXaxis()->GetNbins();
@@ -59,6 +61,11 @@ TimeDifferenceFunction::TimeDifferenceFunction(TCanvas* canvas)
 TimeDifferenceFunction::~TimeDifferenceFunction()
 {}
 
+void TimeDifferenceFunction::draw(TCanvas* canvas)
+{
+  H2DPlot::draw(canvas);
+  histogram()->GetZaxis()->SetRangeUser(-5, 5);
+}
 TF2* TimeDifferenceFunction::function()
 {
   return m_function;
