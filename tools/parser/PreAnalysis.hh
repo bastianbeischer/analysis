d2e70f2c2cff98611e756753eae167518591a4cb
Bastian Beischer <beischer@physik.rwth-aachen.de>
2011-05-05 18:51:08 +0200

    Parser now uses a very simple single thread approach to generate zero suppressed (pre-analyzed) data files directly.

---
+++ tools/parser/PreAnalysis.hh
@@ -0,0 +1,28 @@
+#ifndef PreAnalysis_hh
+#define PreAnalysis_hh
+
+#include <QMap>
+#include <QVector>
+
+class DetectorElement;
+class Hit;
+class SimpleEvent;
+
+typedef QMap< DetectorElement*, QVector<Hit*> > HitStorage;
+typedef QMap< DetectorElement*, QVector<Hit*> >::iterator HitStorageIterator;
+
+class PreAnalysis
+{
+  
+public:
+  PreAnalysis();
+  ~PreAnalysis();
+  
+  SimpleEvent* generateCompressedEvent(SimpleEvent* event);
+
+private:
+  HitStorage m_hitStorage;
+  
+};
+
+#endif /* PreAnalysis_hh */
