ea3cf9131a0aaeaf8019ba0c94555a5e67b50dbf
Bastian Beischer <beischer@physik.rwth-aachen.de>
2011-05-08 09:46:24 +0200

    Eventcounter now counts events in files with corrupted headers, too.

--- tools/eventcounter/main.cc
+++ tools/eventcounter/main.cc
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <QString>
 
+#include <QDebug>
+
 #include "RunFile.h"
 
 int main(int argc, char** argv)
@@ -16,7 +18,25 @@ int main(int argc, char** argv)
   RunFile runfile(input, RunFile::MODE_READING);
 
   int nCalibEvents = runfile.GetNumberOfCalibrationEvents();
-  int nEvents = runfile.GetNumberOfEvents() - runfile.GetNumberOfCalibrationEvents();
+  int nEvents = runfile.GetNumberOfCalibrationEvents() - nCalibEvents;;
+  if (nEvents == 0) {
+    qDebug() << "File doesn't contain a valid header. Trying to read as many events as possible.";
+
+    int counter = 0;
+    RawEvent* event = 0;
+    while(counter < nCalibEvents) {
+      event = (RawEvent*) runfile.ReadNextEvent();
+      delete event;
+      counter++;
+    }
+
+    counter = 0;
+    while((event = (RawEvent*) runfile.ReadNextEvent())) {
+      delete event;
+      counter++;
+    }
+    nEvents = counter;
+  }
 
   QString output = QString("%1 particle events   %2 calibration events").arg(nEvents, 6).arg(nCalibEvents, 6);
   std::cout << qPrintable(output) << std::endl;
