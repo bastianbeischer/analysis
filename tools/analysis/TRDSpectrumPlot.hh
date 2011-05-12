f9b622191a711161c83db4e85426a62379ed007e
David Schug <schug@physik.rwth-aachen.de>
2011-05-12 17:17:55 +0200

    in TRDSpectrumPlot you can set cerenkov limits and the range of cerenkov signals to be used

--- tools/analysis/TRDSpectrumPlot.hh
+++ tools/analysis/TRDSpectrumPlot.hh
@@ -11,8 +11,9 @@ class TRDSpectrumPlot : public AnalysisPlot, public H1DPlot
   
 public:
   enum TRDSpectrumType{completeTRD, module, channel};
+  enum CerenkovCut{bothAbove, bothBelow, c1AboveC2Below, c1BelowC2Above, none};
 
-  TRDSpectrumPlot(unsigned short, TRDSpectrumType, double lowerMomentum = -10, double upperMomentum = 10);
+  TRDSpectrumPlot(unsigned short, TRDSpectrumType, double lowerMomentum = -100, double upperMomentum = 100, CerenkovCut cCut = none, double c1Limit = 200, double c2Limit = 200);
   ~TRDSpectrumPlot();
 
   virtual void processEvent(const QVector<Hit*>&, Particle* = 0, SimpleEvent* = 0);
@@ -29,6 +30,9 @@ private:
   const double m_landauFitRange_upper;
   const double m_lowerMomentum;
   const double m_upperMomentum;
+  const CerenkovCut m_cerenkovCut;
+  const double m_cerenkov1Limit;
+  const double m_cerenkov2Limit;
 
   TMarker* m_fitRangeMarker_lower;
   TMarker* m_fitRangeMarker_upper;
