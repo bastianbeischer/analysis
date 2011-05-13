b4bd1c217120f70fd5c02de8e3f4b6e9e8096906
David Schug <schug@physik.rwth-aachen.de>
2011-05-13 19:22:31 +0200

    small drawing improvements

--- tools/analysis/PMTCorrelationPlot.cc
+++ tools/analysis/PMTCorrelationPlot.cc
@@ -33,17 +33,25 @@ PMTCorrelationPlot::PMTCorrelationPlot() :
   m_line2->SetLineColor(kGreen);
   m_line2->SetLineStyle(2);
 
+  double xMin = histogram->GetXaxis()->GetXmin();
+  double xMax = histogram->GetXaxis()->GetXmax();
+  double yMin = histogram->GetYaxis()->GetXmin();
+  double yMax = histogram->GetYaxis()->GetXmax();
 
-  addLatex(RootPlot::newLatex(.50, .90));
+  addLatex(RootPlot::newLatex(xMin + (xMax-xMin)*.40, yMin + (yMax-yMin)*.98));
 
-  addLatex(RootPlot::newLatex(.80, .85));
-  addLatex(RootPlot::newLatex(.12, .15));
+  addLatex(RootPlot::newLatex(xMin + (xMax-xMin)*.80, yMin + (yMax-yMin)*.95));
+  addLatex(RootPlot::newLatex(xMin + (xMax-xMin)*.02, yMin + (yMax-yMin)*.08));
 
-  addLatex(RootPlot::newLatex(.25, .17));
-  addLatex(RootPlot::newLatex(.13, .30));
+  addLatex(RootPlot::newLatex(m_c1LowerEdgeFirstBinAbove+(xMax-xMin)*.01, yMin + (yMax-yMin)*.10));
+  addLatex(RootPlot::newLatex(xMin + (xMax-xMin)*.03, m_c2LowerEdgeFirstBinAbove + (yMax-yMin)*.05));
 
-  addLatex(RootPlot::newLatex(.12, .85));
-  addLatex(RootPlot::newLatex(.80, .15));
+  addLatex(RootPlot::newLatex(xMin + (xMax-xMin)*.02, yMin + (yMax-yMin)*.95));
+  addLatex(RootPlot::newLatex(xMin + (xMax-xMin)*.80, yMin + (yMax-yMin)*.08));
+
+  for (int i = 0; i < 7; ++i) {
+    latex(i)->SetNDC(kFALSE);
+  }
 
 }
 
@@ -69,13 +77,12 @@ void PMTCorrelationPlot::update()
 {
   m_line1->SetX1(m_c1LowerEdgeFirstBinAbove);
   m_line1->SetX2(m_c1LowerEdgeFirstBinAbove);
-  m_line1->SetY1(histogram()->GetYaxis()->GetXmin());
-  m_line1->SetY2(histogram()->GetYaxis()->GetXmax());
-  m_line2->SetX1(histogram()->GetXaxis()->GetXmin());
-  m_line2->SetX2(histogram()->GetXaxis()->GetXmax());
+  m_line1->SetY1(histogram()->GetYaxis()->GetBinLowEdge(histogram()->GetYaxis()->GetFirst()));
+  m_line1->SetY2(histogram()->GetYaxis()->GetBinUpEdge(histogram()->GetYaxis()->GetLast()));
+  m_line2->SetX1(histogram()->GetXaxis()->GetBinLowEdge(histogram()->GetXaxis()->GetFirst()));
+  m_line2->SetX2(histogram()->GetXaxis()->GetBinUpEdge(histogram()->GetXaxis()->GetLast()));
   m_line2->SetY1(m_c2LowerEdgeFirstBinAbove);
   m_line2->SetY2(m_c2LowerEdgeFirstBinAbove);
-
 
   double allEvents = histogram()->GetEntries();
   latex(0)->SetTitle(qPrintable(QString("%1 entries").arg(allEvents)));
@@ -103,21 +110,4 @@ void PMTCorrelationPlot::update()
   double aboveC1belowC2Entries = histogram()->Integral(m_c1FirstBinAbove, histogram()->GetXaxis()->GetNbins()
                                                   , 0, m_c2FirstBinAbove-1);
   latex(6)->SetTitle(qPrintable(QString("%1\%").arg(100. * aboveC1belowC2Entries / allEvents, 5, 'f', 2)));
-
-
-
-
-
-  //double aboveBothEntries = histogram()->Integral(m_c1LowerEdgeFirstBinAbove, histogram()->GetXaxis()->GetNbins());
-
-  /*double sum0 = 0;
-  double sum1 = 0;
-  int nBins = histogram()->GetNbinsX();
-  for (int bin = 1; bin <= nBins; ++bin) {
-    double content = histogram()->GetBinContent(bin);
-    sum0+= content;
-    if (bin > m_thresholdBin)
-      sum1+= content;
-  }
-  latex()->SetTitle(qPrintable(QString("%1\%").arg(100. * sum1 / sum0, 5, 'f', 2)));*/
 }
