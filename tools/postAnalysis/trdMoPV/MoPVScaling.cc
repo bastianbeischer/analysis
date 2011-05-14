aaa3f177cb511a70802ac6f23ce21cfe6bd58bd7
David Schug <schug@physik.rwth-aachen.de>
2011-05-14 06:16:46 +0200

    trdMoPV postAnalysis tool now shows mopvs vs time/temperature for the whole trd and for individual modules

--- tools/postAnalysis/trdMoPV/MoPVScaling.cc
+++ tools/postAnalysis/trdMoPV/MoPVScaling.cc
@@ -8,23 +8,42 @@
 #include <TF1.h>
 #include <TLatex.h>
 
-#include <QDebug>
+#include <QList>
 
 #include <math.h>
 
 MoPVScaling::MoPVScaling(PostAnalysisCanvas* canvas)
   : PostAnalysisPlot()
   , GraphPlot()
+  , m_colorCounter(0)
 {
   TH2D* histogram = canvas->histograms2D().at(0);
-  m_mopvGraph = generateMoPVGraph(histogram);
-
+  TGraph* graph = generateMoPVGraph(histogram);
   QString title = QString(canvas->name()).replace("canvas", "graph");
   setTitle(title);
-  addGraph(m_mopvGraph, "P");
-
+  addGraph(graph, "L");
   setAxisTitle("time", "trd mopv");
-  
+}
+
+MoPVScaling::MoPVScaling(QList<PostAnalysisCanvas*> canvasList)
+  : PostAnalysisPlot()
+  , GraphPlot()
+  , m_colorCounter(0)
+{
+  //TODO is first name ok?
+  QString title = QString(canvasList.first()->name()).replace("canvas", "graph");
+  setTitle(title);
+  setAxisTitle("time", "trd mopv");
+
+  foreach (PostAnalysisCanvas* canvas, canvasList) {
+    TH2D* histogram = canvas->histograms2D().at(0);
+    TGraph* graph = generateMoPVGraph(histogram);
+    graph->SetLineColor(RootStyle::rootColor(m_colorCounter++));
+    graph->SetLineWidth(1);
+    graph->SetDrawOption("LX");
+    addGraph(graph, "LX");
+  }
+  drawOption(ALX);
 }
 
 MoPVScaling::~MoPVScaling()
@@ -39,23 +58,37 @@ TGraphErrors* MoPVScaling::generateMoPVGraph(TH2D* histogram)
   for (int bin = 1; bin < histogram->GetNbinsX()+1; ++bin) {
     TH1* projectionHistogram = histogram->ProjectionY("_py", bin, bin);
     int nEntries = projectionHistogram->GetEntries();
-    if (nEntries < 30)
+    if (nEntries < 100)
         continue;
     //which fit range ?!?, use same as TRD spectrum ?!?
-    TF1* function = new TF1(qPrintable(QString(histogram->GetTitle()) + "Function"), "landau", histogram->GetYaxis()->GetXmin(), histogram->GetYaxis()->GetXmax());
-    projectionHistogram->Fit(function, "EQN0");
+    TF1* functionLandau = new TF1(qPrintable(QString(histogram->GetTitle()) + "FunctionLandau"), "landau", histogram->GetYaxis()->GetXmin(), histogram->GetYaxis()->GetXmax());
+    projectionHistogram->Fit(functionLandau, "EQN0");
 
-    double mpv = function->GetParameter(1);
-    double mpvErr = function->GetParError(1);
+    double mpv = functionLandau->GetParameter(1);
+    double mpvErr = functionLandau->GetParError(1);
+
+    //fit again:
+    /*
+    projectionHistogram->Fit(functionLandau, "EQN0R", 0, mpv-0.5*mpv, mpv+1.5*mpv);
+    mpv = functionLandau->GetParameter(1);
+    mpvErr = functionLandau->GetParError(1)
+
+    TF1* functionGaus = new TF1(qPrintable(QString(histogram->GetTitle()) + "FunctionGaus"), "gaus", mpv-0.2*mpv, mpv+0.2*mpv);
+    projectionHistogram->Fit(functionGaus, "EQN0");
+    double mpvGaus = functionGaus->GetParameter(1);
+    double mpvGausErr = functionGaus->GetParError(1);
+    delete functionGaus;
+    */
 
     delete projectionHistogram;
-    delete function;
+    delete functionLandau;
 
-    double time = histogram->GetXaxis()->GetBinCenter(bin);
-    double timeErr = histogram->GetXaxis()->GetBinWidth(bin)/sqrt(12);
+
+    double xValue = histogram->GetXaxis()->GetBinCenter(bin);
+    double xValueErr = 0; //histogram->GetXaxis()->GetBinWidth(bin)/sqrt(12);
     
-    x << time;
-    xErr << timeErr;
+    x << xValue;
+    xErr << xValueErr;
     y << mpv;
     yErr << mpvErr;
   }
--- tools/postAnalysis/trdMoPV/MoPVScaling.hh
+++ tools/postAnalysis/trdMoPV/MoPVScaling.hh
@@ -4,18 +4,24 @@
 #include "PostAnalysisPlot.hh"
 #include "GraphPlot.hh"
 
+#include <QList>
+
 class PostAnalysisCanvas;
+class TMultiGraph;
 class TGraphErrors;
 class TH2D;
 
 class MoPVScaling : public PostAnalysisPlot, public GraphPlot {
 public:
   MoPVScaling(PostAnalysisCanvas*);
+  MoPVScaling(QList<PostAnalysisCanvas*>);
   virtual ~MoPVScaling();
   
 private:
-  TGraphErrors* m_mopvGraph;
+  QList<TGraphErrors*> m_mopvGraphs;
   TGraphErrors* generateMoPVGraph(TH2D* histogram);
+
+  int m_colorCounter;
 
 };
 
