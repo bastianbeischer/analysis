077eb5f20291aac4700dee28c51d5744ef883f4d
David Schug <schug@physik.rwth-aachen.de>
2011-05-21 00:15:28 +0200

    first version of testbeam corrections, const factors mopvs normalised at -10GeV to 1

--- bin/switch_to_testbeam_conf.sh
+++ bin/switch_to_testbeam_conf.sh
@@ -1,5 +1,5 @@
 echo "switching config files to testbeam configuration"
 DIR=${PERDAIXANA_PATH}/conf
 cp -f ${DIR}/testbeam_setup.conf ${DIR}/setup.conf
-#cp -f ${DIR}/testbeam_TRDCorrections.conf ${DIR}/TRDCorrections.conf  
+cp -f ${DIR}/testbeam_TRDCorrections.conf ${DIR}/TRDCorrections.conf  
 echo "done"
