c87098a60940fec6e0327e69baa3f0d360beed69
Bastian Beischer <beischer@physik.rwth-aachen.de>
2011-06-06 14:29:49 +0200

    Settings can store cherenkov pressures now.

--- setup/Settings.cc
+++ setup/Settings.cc
@@ -1,5 +1,7 @@
 #include "Settings.hh"
 
+#include <cmath>
+
 Settings::Settings() :
   m_situation(Unknown),
   m_identifier(0),
@@ -8,6 +10,7 @@ Settings::Settings() :
   m_lastRun(0),
   m_momentum(0),
   m_polarity(0),
+  m_pressure(0.),
   m_magnet(false)
 {
 }
