4e8d30b35c29c166a0afe2d9b902ec151f9c2d11
David Schug <schug@physik.rwth-aachen.de>
2011-07-03 22:31:11 +0200

    all functions

--- tools/analysis/TRDLikelihoodFunctionsPlot.cc
+++ tools/analysis/TRDLikelihoodFunctionsPlot.cc
@@ -16,6 +16,13 @@ TRDLikelihoodFunctionsPlot::TRDLikelihoodFunctionsPlot()
 
   TRDLikelihoods* lhs = TRDLikelihoods::instance();
   addFunction(new TF1(*lhs->getLHFunctionNonTR()));
+  addFunction(new TF1(*lhs->getLHFunctionTR()));
+  function(1)->SetLineColor(kRed);
+  for (int i = 0; i < 8; ++i) {
+    addFunction(new TF1(*lhs->getLHFunctionTRLayer(i)));
+    function(i+2)->SetLineWidth(0.1);
+    function(i+2)->SetLineColor(kMagenta-4+(i%2)*4+i);
+  }
 }
 
 TRDLikelihoodFunctionsPlot::~TRDLikelihoodFunctionsPlot()
