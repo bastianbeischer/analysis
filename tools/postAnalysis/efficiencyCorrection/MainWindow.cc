cac81b3c0410a3fde2c64246ff49f66ea0996b06
Andreas Bachlechner <bachlechner@physik.rwth-aachen.de>
2011-07-11 10:47:18 +0200

    added postAnalysis tool for efficiency corrections

---
+++ tools/postAnalysis/efficiencyCorrection/MainWindow.cc
@@ -0,0 +1,34 @@
+#include "MainWindow.hh"
+
+#include "PostAnalysisPlot.hh"
+#include "PostAnalysisCanvas.hh"
+#include "TrackingEfficiencyCorrection.hh"
+
+#include <TCanvas.h>
+#include <TFile.h>
+#include <TROOT.h>
+
+#include <QDebug>
+#include <QFileDialog>
+
+MainWindow::MainWindow(QWidget* parent)
+  : PostAnalysisWindow(parent)
+{
+}
+
+MainWindow::~MainWindow()
+{
+}
+
+void MainWindow::setupAnalysis()
+{
+  PostAnalysisCanvas* canvas = 0;
+  TFile file(qPrintable(m_analysisFile));
+  gROOT->cd();
+  
+  QString name = QString("Multi Layer Efficiency canvas");
+  canvas = addCanvas(&file, qPrintable(name));
+  addPlot(new TrackingEfficiencyCorrection(canvas));
+  
+  file.Close();
+}
