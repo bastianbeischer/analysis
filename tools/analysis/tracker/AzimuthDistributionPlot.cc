2a11d75b5038c7af4718b16c4a1e5a628e678928
Andreas Bachlechner <bachlechner@physik.rwth-aachen.de>
2011-07-21 13:41:48 +0200

    added comparison of monte carlo and measurement to distribiution plot

--- tools/analysis/tracker/AzimuthDistributionPlot.cc
+++ tools/analysis/tracker/AzimuthDistributionPlot.cc
@@ -1,7 +1,11 @@
 #include "AzimuthDistributionPlot.hh"
 
+#include <QDebug>
+
 #include <TH1D.h>
 #include <TLatex.h>
+#include <TFile.h>
+#include <TROOT.h>
 
 #include "Hit.hh"
 #include "ParticleInformation.hh"
@@ -22,13 +26,17 @@ H1DPlot()
   double xMax = 180;
   TH1D* histogram = new TH1D(qPrintable(title()), "", nBins, xMin, xMax);
   histogram->Sumw2();
-  setAxisTitle("azimuth", "entries");
+  setAxisTitle("azimuth", "probability / %");
   addHistogram(histogram);
   addLatex(RootPlot::newLatex(.15, .85));
+  
+  readFile();
+  addHistogram(m_azimuthAcceptance, H1DPlot::HIST);
 }
 
 AzimuthDistributionPlot::~AzimuthDistributionPlot()
 {
+
 }
 
 void AzimuthDistributionPlot::processEvent(const QVector<Hit*>&, const Particle* const particle, const SimpleEvent* const)
@@ -49,5 +57,46 @@ void AzimuthDistributionPlot::processEvent(const QVector<Hit*>&, const Particle*
 
 void AzimuthDistributionPlot::update()
 {
+  double integral = histogram(0)->Integral("width");
+  histogram(0)->Scale(100/integral);
+}
+
+void AzimuthDistributionPlot::finalize()
+{
+  update();
+}
+
+void AzimuthDistributionPlot::readFile()
+{
+  QString filename = "azimuthAcceptance.root";
+  std::cout << "Reading file " <<qPrintable(filename) << std::endl;
+  QString hName = "phiHistoValid";
+  TFile* openfile = new TFile(qPrintable(filename));
+  gROOT->cd();
+  TH1D* azimuthAcceptance = (TH1D*)(((TH1D*)openfile->Get(qPrintable(hName)))->Clone());
+  double integral = azimuthAcceptance->Integral("width");
+  azimuthAcceptance->Scale(100/integral);
+  openfile->Close();
+  delete openfile;
+  openfile = 0;
   
+  m_azimuthAcceptance = new TH1D("acceptance phi", "", azimuthAcceptance->GetNbinsX(), -180, 180);
+  for (int i = 0; i < m_azimuthAcceptance->GetNbinsX(); ++i) {
+    double azimuth = m_azimuthAcceptance->GetBinCenter(i+1);
+    int bin = azimuthAcceptance->FindBin(angleMovement(azimuth));
+    double content = azimuthAcceptance->GetBinContent(bin);
+    m_azimuthAcceptance->SetBinContent(i, content);
+  }
+  delete azimuthAcceptance;
+  azimuthAcceptance = 0;
+  m_azimuthAcceptance->SetMarkerColor(kBlue);
+  m_azimuthAcceptance->SetLineColor(kBlue);
+}
+
+double AzimuthDistributionPlot::angleMovement(double azimuth)
+{
+  double newAzimuth = azimuth + 90;
+  if (azimuth < -90)
+    newAzimuth += 360;
+  return newAzimuth;
 }
--- tools/analysis/tracker/AzimuthDistributionPlot.hh
+++ tools/analysis/tracker/AzimuthDistributionPlot.hh
@@ -14,9 +14,12 @@ public:
   
   void processEvent(const QVector<Hit*>&, const Particle* const = 0, const SimpleEvent* const = 0);
   void update();
+  void finalize();
 
 private:
-
+  void readFile();
+  double angleMovement(double azimuth);
+  TH1D* m_azimuthAcceptance;
 };
 
 #endif /* AzimuthDistributionPlot_hh */
