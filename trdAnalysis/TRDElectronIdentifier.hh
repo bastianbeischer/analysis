40d8031041269752902045197021764a08d97271
Bastian Beischer <beischer@physik.rwth-aachen.de>
2011-07-25 18:30:20 +0200

    trdAnalysis: Indentation fixes

--- trdAnalysis/TRDElectronIdentifier.hh
+++ trdAnalysis/TRDElectronIdentifier.hh
@@ -10,7 +10,7 @@ class Particle;
 class TRDElectronIdentifier
 {
 public:
-    virtual bool isElectronish(const QVector<Hit*>& hits, const Particle* particle, const SimpleEvent* event, bool &ok) = 0;
+  virtual bool isElectronish(const QVector<Hit*>& hits, const Particle* particle, const SimpleEvent* event, bool &ok) = 0;
 };
 
 #endif // TRDELECTRONIDENTIFIER_HH
--- trdAnalysis/TRDElectronIdentifierCutBased.hh
+++ trdAnalysis/TRDElectronIdentifierCutBased.hh
@@ -6,12 +6,12 @@
 class TRDElectronIdentifierCutBased : public TRDElectronIdentifier
 {
 public:
-    TRDElectronIdentifierCutBased();
-    bool isElectronish(const QVector<Hit*>& hits, const Particle* particle, const SimpleEvent* event, bool &ok);
+  TRDElectronIdentifierCutBased();
+  bool isElectronish(const QVector<Hit*>& hits, const Particle* particle, const SimpleEvent* event, bool &ok);
 private:
-    unsigned int m_layerCut;
-    double m_energyDepositionCut;
-    double m_energyDepositionPerLengthCut;
+  unsigned int m_layerCut;
+  double m_energyDepositionCut;
+  double m_energyDepositionPerLengthCut;
 };
 
 #endif // TRDELECTRONIDENTIFIERCUTBASED_HH
