a3f506917476b0ba868ee194abc936bbeca95e90
Andreas Bachlechner <bachlechner@physik.rwth-aachen.de>
2011-11-03 15:55:04 +0100

    in SignalHeight2DPlot the calculation of the normilization is done during update

--- tools/analysis/SignalHeight2DPlot.cc
+++ tools/analysis/SignalHeight2DPlot.cc
@@ -13,13 +13,13 @@
 #include <TH2D.h>
 #include <TH1D.h>
 
-
 #include <iostream>
 
-SignalHeight2DPlot::SignalHeight2DPlot() :
-  AnalysisPlot(Enums::SignalHeightTracker),
-  H2DProjectionPlot(),
-  m_normHisto(0)
+SignalHeight2DPlot::SignalHeight2DPlot()
+:  AnalysisPlot(Enums::SignalHeightTracker)
+,  H2DProjectionPlot()
+,  m_histo(0)
+,  m_normHisto(0)
 {
   controlWidget()->spinBox()->setMaximum(200);
   Setup* setup = Setup::instance();
@@ -33,22 +33,21 @@ SignalHeight2DPlot::SignalHeight2DPlot() :
       counter++;
     }
   }
-
   setTitle("signal height 2d");
   TH2D* histo = new TH2D(qPrintable(title()), "", counter, -0.5, counter - 0.5, 100, 0, 4096);
+  m_histo = new TH2D(*histo);
   addHistogram(histo);
-  
   for (QMap<unsigned short, int>::iterator it = m_indexMap.begin(); it != m_indexMap.end(); it++) {
     unsigned short id = it.key();
     int channel = it.value();
     xAxis()->SetBinLabel(channel + 1, qPrintable(QString("0x%1").arg(id, 0, 16)));
   }
-
   m_normHisto = new TH1D(qPrintable(title() + "_norm"), "", counter, -0.5, counter - 0.5);
 }
 
 SignalHeight2DPlot::~SignalHeight2DPlot()
 {
+  delete m_histo;
   delete m_normHisto;
 }
 
@@ -68,20 +67,20 @@ void SignalHeight2DPlot::processEvent(const QVector<Hit*>&, const Particle* cons
     Hit* hit = *it;
     if (hit->type() == Hit::tracker) {
       unsigned short id = hit->elementId();
-      histogram()->Fill(m_indexMap[id], hit->signalHeight());
+      m_histo->Fill(m_indexMap[id], hit->signalHeight());
       m_normHisto->Fill(m_indexMap[id]);
     }
   }
 }
 
-void SignalHeight2DPlot::finalize()
+void SignalHeight2DPlot::update()
 {
-  TH2D* hist = histogram();
+  TH2D* hist = m_histo;
   for (int xBin = 1; xBin <= hist->GetNbinsX(); xBin++) {
     for (int yBin = 1; yBin <= hist->GetNbinsY(); yBin++) {
       double bc = hist->GetBinContent(xBin, yBin);
       double norm = m_normHisto->GetBinContent(xBin);
-      bc > 10 ? hist->SetBinContent(xBin, yBin, bc/norm) : hist->SetBinContent(xBin, yBin, 0.);
+      bc > 10 ? histogram()->SetBinContent(xBin, yBin, bc/norm) : histogram()->SetBinContent(xBin, yBin, 0.);
     }
   }
 }
