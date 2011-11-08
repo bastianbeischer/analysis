dedc0447e8e5e7b6fd9787bbaa694934c5ca9b5c
Andreas Bachlechner <bachlechner@physik.rwth-aachen.de>
2011-11-08 13:33:40 +0100

    change in mean calculation

--- tools/postAnalysis/signalHeightScaling/SignalHeightTempCor.hh
+++ tools/postAnalysis/signalHeightScaling/SignalHeightTempCor.hh
@@ -5,29 +5,15 @@
 #include "PostAnalysisPlot.hh"
 #include "GraphPlot.hh"
 
-#include <QObject>
-#include <QString>
-#include <QVector>
-#include <QMap>
-
 #include <TGraphErrors.h>
-#include <TF1.h>
-#include <TH2D.h>
 
-class SignalHeightTempCor : public QObject, public PostAnalysisPlot, public GraphPlot {
-  Q_OBJECT
+class SignalHeightTempCor : public PostAnalysisPlot, public GraphPlot {
 public:
   SignalHeightTempCor(PostAnalysisCanvas*, unsigned short sipmId);
   virtual ~SignalHeightTempCor();
 private:
-  void fit(unsigned short sipmId, TH2D*);
-  void save(unsigned short sipmId);
-  unsigned int m_sipmId;
-  TGraphErrors m_graph;
-  static QMap<unsigned short, TF1> s_SignalHeightTempCorFits;
-private slots:
-  void save();
-  void saveAll();
+  unsigned short m_sipmId;
+  TGraphErrors* meanGraph(TH2D* histogram);
 };
 
 #endif
--- tools/postAnalysis/signalHeightScaling/SignalHeightTimeCor.hh
+++ tools/postAnalysis/signalHeightScaling/SignalHeightTimeCor.hh
@@ -7,11 +7,8 @@
 
 #include <QObject>
 #include <QString>
-#include <QVector>
-#include <QMap>
 
 #include <TGraphErrors.h>
-#include <TF1.h>
 #include <TH2D.h>
 
 class SignalHeightTimeCor : public QObject, public PostAnalysisPlot, public GraphPlot {
@@ -20,11 +17,9 @@ public:
   SignalHeightTimeCor(PostAnalysisCanvas*, unsigned short sipmId);
   virtual ~SignalHeightTimeCor();
 private:
-  void fit(unsigned short sipmId, TH2D*);
+  TGraphErrors* meanGraph(unsigned short sipmId, TH2D*);
   void save(unsigned short sipmId);
   unsigned int m_sipmId;
-  TGraphErrors m_graph;
-  static QMap<unsigned short, TF1> s_SignalHeightTimeCorFits;
 private slots:
   void save();
   void saveAll();
