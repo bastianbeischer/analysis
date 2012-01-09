785e89cb7d9acb6404aa957db398966830721003
Andreas Bachlechner <bachlechner@physik.rwth-aachen.de>
2012-01-09 10:50:12 +0100

    fixed absenteeism of tof time over threshold in the analysis software. temperatures for mc are not valid and have to be captured.

--- processing/Corrections.cc
+++ processing/Corrections.cc
@@ -603,6 +603,8 @@ void Corrections::loadTotScaling()
 
 double Corrections::totScalingFactor(const unsigned int tofId, const double temperature)
 {
+  if (std::isnan(temperature))
+    return 1;
   double value = m_totScalings[tofChannel(tofId)][0] + m_totScalings[tofChannel(tofId)][1] * temperature;
   return (value == 0) ? 0 : Constants::idealTot / value;
   //TODO: some kind of information if the temperature value is valid for this scaling
