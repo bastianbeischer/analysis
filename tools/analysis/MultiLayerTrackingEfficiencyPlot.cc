1f3a4c7f5cc12c89cf8986d3e22fbbb28399b3a2
Bastian Beischer <beischer@physik.rwth-aachen.de>
2012-04-08 17:01:28 +0200

    tools/analysis: Removed compiler warning in MultiLayerTrackingEfficiencyPlot.cc.
    Variable is not needed there anymore.

--- tools/analysis/MultiLayerTrackingEfficiencyPlot.cc
+++ tools/analysis/MultiLayerTrackingEfficiencyPlot.cc
@@ -76,7 +76,7 @@ MultiLayerTrackingEfficiencyPlot::~MultiLayerTrackingEfficiencyPlot()
 void MultiLayerTrackingEfficiencyPlot::processEvent(const AnalyzedEvent* event)
 {
   const Track* track = event->particle()->track();
-  Enums::EventFlags flags = event->particle()->information()->eventFlags();
+
   if (!event->flagsSet(Enums::Chi2Good | Enums::InsideMagnet))
     return;
   double rigidity = event->particle()->hypothesis()->rigidity();
