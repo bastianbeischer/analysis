622401af8c7208697afbae09bf5b50d23b907b46
Roman Greim <greim@physik.rwth-aachen.de>
2012-05-29 18:11:58 +0200

    Made cloneClass.sh a bit mor flexible.

--- bin/cloneClass.sh
+++ bin/cloneClass.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-ORIG_CLASS=$(basename $1 | sed -e "s/\.$//g" | sed -e "s/\.cc$//g" | sed -e "s/\.hh$//g")
+HEADER_ENDING=h
+SOURCE_ENDING=cpp
+ORIG_CLASS=$(basename $1 | sed -e "s/\.$//g" | sed -e "s/\.${SOURCE_ENDING}$//g" | sed -e "s/\.${HEADER_ENDING}$//g")
 ORIG_PATH=$(dirname $1)
-DEST_CLASS=$(basename $2 | sed -e "s/\.$//g" | sed -e "s/\.cc$//g" | sed -e "s/\.hh$//g")
+DEST_CLASS=$(basename $2 | sed -e "s/\.$//g" | sed -e "s/\.${SOURCE_ENDING}$//g" | sed -e "s/\.${HEADER_ENDING}$//g")
 DEST_PATH=$(dirname $2)
-echo "${ORIG_PATH}/${ORIG_CLASS}.hh -> ${DEST_PATH}/${DEST_CLASS}.hh"
-cat ${ORIG_PATH}/${ORIG_CLASS}.hh | sed -e "s/${ORIG_CLASS}/${DEST_CLASS}/g" >> ${DEST_PATH}/${DEST_CLASS}.hh
-echo "${ORIG_PATH}/${ORIG_CLASS}.cc -> ${DEST_PATH}/${DEST_CLASS}.cc"
-cat ${ORIG_PATH}/${ORIG_CLASS}.cc | sed -e "s/${ORIG_CLASS}/${DEST_CLASS}/g" >> ${DEST_PATH}/${DEST_CLASS}.cc
+echo "${ORIG_PATH}/${ORIG_CLASS}.${HEADER_ENDING} -> ${DEST_PATH}/${DEST_CLASS}.${HEADER_ENDING}"
+cat ${ORIG_PATH}/${ORIG_CLASS}.${HEADER_ENDING} | sed -e "s/${ORIG_CLASS}/${DEST_CLASS}/g" >> ${DEST_PATH}/${DEST_CLASS}.${HEADER_ENDING}
+echo "${ORIG_PATH}/${ORIG_CLASS}.${SOURCE_ENDING} -> ${DEST_PATH}/${DEST_CLASS}.${SOURCE_ENDING}"
+cat ${ORIG_PATH}/${ORIG_CLASS}.${SOURCE_ENDING} | sed -e "s/${ORIG_CLASS}/${DEST_CLASS}/g" >> ${DEST_PATH}/${DEST_CLASS}.${SOURCE_ENDING}
