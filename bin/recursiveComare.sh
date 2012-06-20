10c990645a93f3854387b12c6db9f9ac008b81a6
Roman Greim <greim@physik.rwth-aachen.de>
2012-06-20 19:11:44 +0200

    directly print rsync out put. Avoids trouble with skipping non-regular file messages.

--- bin/recursiveComare.sh
+++ bin/recursiveComare.sh
@@ -13,24 +13,13 @@ fi
 PATH1=$(cd ${1}; pwd; cd - > /dev/null)
 PATH2=$(cd ${2}; pwd; cd - > /dev/null)
 
-MISSING_FILES_1=$(rsync --prune-empty-dirs -rnvc --ignore-existing ${PATH1}/ ${PATH2}/ | tail -n +2 | head -n -3)
 echo ""
 echo "Missing files in ${PATH2}:"
-for FILE in ${MISSING_FILES_1} ; do
-  echo $FILE
-done
-  
-MISSING_FILES_2=$(rsync --prune-empty-dirs -rnvc --ignore-existing ${PATH2}/ ${PATH1}/ | tail -n +2 | head -n -3)
+rsync --prune-empty-dirs -rnvc --ignore-existing ${PATH1}/ ${PATH2}/ | tail -n +2 | head -n -3
 echo ""
 echo "Missing files in ${PATH1}:"
-for FILE in ${MISSING_FILES_2} ; do
-  echo $FILE
-done
-
-DIFFERING_FILES=$(rsync --prune-empty-dirs -rnvc --existing ${PATH2}/ ${PATH1}/ | tail -n +2 | head -n -3)
+rsync --prune-empty-dirs -rnvc --ignore-existing ${PATH2}/ ${PATH1}/ | tail -n +2 | head -n -3
 echo ""
 echo "Differing files:"
-for FILE in ${DIFFERING_FILES} ; do
-  echo $FILE
-done
+rsync --prune-empty-dirs -rnvc --existing ${PATH2}/ ${PATH1}/ | tail -n +2 | head -n -3
 echo ""
